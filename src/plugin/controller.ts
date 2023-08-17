defed57bd6cf3c6cc72689ff895af3f81747f9e2
AnyWayDude <zpaulz61@gmail.com>
2023-08-17 11:29:39 +0200

    del: clear plugin

--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -8,41 +8,17 @@ figma.on('selectionchange', () => {
 
   const textNodes = selectedObjects.reduce((acc: TextNode[], object) => {
     acc.push(...getAllTextNodes(object));
-
     return acc;
   }, []);
 
-  // const textNodesMap = selectedObjects.map((item) => getAllTextNodes(item)).flat();
+  const frameName = selectedObjects && selectedObjects[0] ? selectedObjects[0].name : null;
 
-  if (textNodes.length > 0) {
-    const filteredTextNodes = textNodes.filter((textNode) => {
-      if (typeof textNode.fontSize === 'number') {
-        const fontSize = textNode.fontSize;
-        return fontSize < 10;
-      }
-      return false;
-    });
-    console.log('отфильтрованный', filteredTextNodes);
-    console.log('нефильтрованный', textNodes);
-
-    figma.ui.postMessage({
-      type: 'display-text-nodes',
-      frameName: selectedObjects[0].name,
-      textNodes: filteredTextNodes.map(({ id, name, characters }) => {
-        return { id, name, text: characters };
-      }),
-    });
-
-    figma.ui.onmessage = (msg) => {
-      if (msg.type === 'scanFrame') {
-        filteredTextNodes.forEach(({ characters: text, name }) => {
-          console.log(name);
-
-          console.log('Text:', text);
-        });
-      } else {
-        console.log('No text nodes selected');
-      }
-    };
-  }
+  figma.ui.postMessage({
+    //send message to UI
+    type: 'display-text-nodes',
+    frameName: frameName,
+    textNodes: textNodes.map(({ id, name, characters, fontSize }) => {
+      return { id, name, text: characters, fontSize };
+    }),
+  });
 });
